27f1dcdd0875299d48437be773e1ff5410f90eeb
saykot biswas shawon <shawon.saykot2023@gmail.com>
2025-09-06 03:22:29 +0600

    Fix: Connect frontend to correct server URL

--- client/src/redux/apis/APIService.js
+++ client/src/redux/apis/APIService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Updated to use Vercel server URL
 export const apiService = axios.create({
-  baseURL: 'https://hr-set2-server-phi.vercel.app',
+  baseURL: 'https://hr-set2-server.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   },
