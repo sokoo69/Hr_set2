3391c0a381846e70d5942e02bb37e505504334b7
saykot biswas shawon <shawon.saykot2023@gmail.com>
2025-09-06 18:09:16 +0600

    Update API service URL for Heroku deployment

--- client/src/redux/apis/APIService.js
+++ client/src/redux/apis/APIService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Updated to use Heroku server URL
 export const apiService = axios.create({
   baseURL: process.env.NODE_ENV === 'production' 
-    ? "https://your-heroku-app-name.herokuapp.com" 
+    ? "https://hr-management-system-shawon-04fce1431e33.herokuapp.com" 
     : "http://localhost:5000",
   headers: {
     'Content-Type': 'application/json',
