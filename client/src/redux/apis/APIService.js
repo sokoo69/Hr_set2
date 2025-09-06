dbc2c6873f12cef74ec9b1fbda8386ae660d4ecc
saykot biswas shawon <shawon.saykot2023@gmail.com>
2025-09-06 14:21:28 +0600

    Configure monorepo: Use same domain for frontend and backend

--- client/src/redux/apis/APIService.js
+++ client/src/redux/apis/APIService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Updated to use Vercel server URL
 export const apiService = axios.create({
-  baseURL: 'https://hr-set2-server.vercel.app',
+  baseURL: 'https://hr-set2-client.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   },
